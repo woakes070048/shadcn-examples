2e71caacdbf24294f93a2fdede47f841a9f8f434
toby <hello@tobybelhome.com>
2025-10-28 02:04:51 +0300

    chore: new demo examples

---
+++ examples/bento-grid/page.tsx
@@ -0,0 +1,25 @@
+import {
+  Empty,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyMedia,
+  EmptyTitle
+} from "@/components/ui/empty";
+import { LoaderCircleIcon } from "lucide-react";
+
+export default function Page() {
+  return (
+    <Empty>
+      <EmptyHeader>
+        <EmptyMedia variant="icon">
+          <LoaderCircleIcon className="-ms-1 animate-spin" size={16} />
+        </EmptyMedia>
+      </EmptyHeader>
+      <EmptyTitle>Coming Soon</EmptyTitle>
+      <EmptyDescription>
+        It is a layout style that arranges content blocks of different sizes in a visually balanced
+        grid.
+      </EmptyDescription>
+    </Empty>
+  );
+}
