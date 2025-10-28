d631c13ec4840ee0bb33b3a995b17e1be89989c3
toby <hello@tobybelhome.com>
2025-10-28 14:22:29 +0300

    chore: links to what's coming soon

---
+++ examples/directory-list/page.tsx
@@ -0,0 +1,25 @@
+import {
+  Empty,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyMedia,
+  EmptyTitle
+} from "@/components/ui/empty";
+import { LoaderCircleIcon } from "lucide-react";
+
+export default function Page() {
+  return (
+    <Empty>
+      <EmptyHeader>
+        <EmptyMedia variant="icon">
+          <LoaderCircleIcon className="-ms-1 animate-spin" size={16} />
+        </EmptyMedia>
+      </EmptyHeader>
+      <EmptyTitle>Coming Soon</EmptyTitle>
+      <EmptyDescription>
+        A directory list is a structured display of items, users, or organizations organized for
+        easy browsing and access.
+      </EmptyDescription>
+    </Empty>
+  );
+}
---
+++ examples/login-pages/page.tsx
@@ -0,0 +1,25 @@
+import {
+  Empty,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyMedia,
+  EmptyTitle
+} from "@/components/ui/empty";
+import { LoaderCircleIcon } from "lucide-react";
+
+export default function Page() {
+  return (
+    <Empty>
+      <EmptyHeader>
+        <EmptyMedia variant="icon">
+          <LoaderCircleIcon className="-ms-1 animate-spin" size={16} />
+        </EmptyMedia>
+      </EmptyHeader>
+      <EmptyTitle>Coming Soon</EmptyTitle>
+      <EmptyDescription>
+        These are authentication forms and screens where users enter their credentials to access
+        their accounts.
+      </EmptyDescription>
+    </Empty>
+  );
+}
